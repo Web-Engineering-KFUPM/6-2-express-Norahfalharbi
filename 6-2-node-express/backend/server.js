5b6b7a62a9122016d0dc4093ef83f647ba864758
Norahfalharbi <s202249220@kfupm.edu.sa>
2025-11-10 11:42:41 +0300

    DONE

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -3,9 +3,21 @@ import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
 
 {/*write code for cors*/}
+const app = express()
+const PORT = 3000;
+app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
 
 
 {/*write code to define routes*/}
-
+app.use(cors());
 
 {/*write code to create server*/}
+app.get("/", (req, res) => {
+      res.send("Welcome to the Quote Generator API");
+    });
+app.get("/api/quote", (req, res) => {
+      const quote = getRandomQuote();
+      res.json({ quote });
+    });
